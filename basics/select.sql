7fcd58933591cb9ea2986c0823da50c8fcb39e19
Abhay Rawat <abhaysinghrawat9411@gmail.com>
2026-07-05 21:14:17 +0530

    changed file structure and did 3rd lesson

--- basics/select.sql
+++ basics/select.sql
@@ -1,3 +1,5 @@
+-- SQL Lesson 1: SELECT queries
+
 CREATE DATABASE sql_practice; 
 
 use sql_practice; 
@@ -46,35 +48,3 @@ SELECT director from Movies; -- 2nd
 SELECT title, director from Movies;  -- 3rd
 SELECT title, release_year from Movies; -- 4th
 SELECT * FROM Movies;  -- 5th
-
-
--- Exercise 2 — Tasks
--- Find the movie with a row id of 6
--- Find the movies released in the years between 2000 and 2010
--- Find the movies not released in the years between 2000 and 2010
--- Find the first 5 Pixar movies and their release year
-
--- solutions 
--- 1st: syntax= SELECT columns FROM table_name WHERE condition;
-select * 
-FROM Movies 
-WHERE id=6;   
--- 2nd 
-SELECT * 
-FROM Movies 
-WHERE release_year >= 2000 
-and release_year <= 2010;
-SELECT *	
-FROM Movies
-WHERE release_year BETWEEN 2000 AND 2010;  -- simpler way 
--- 3rd 
-SELECT * 
-FROM Movies 
-WHERE release_year < 2000 
-or release_year > 2010;	-- simpler way 
--- 4TH
-SELECT title, release_year  -- Show only these two columns.
-FROM Movies		-- Use the Movies table.
-ORDER BY release_year	-- Sort from oldest year to newest year
-LIMIT 5; 	-- Return only the first 5 rows after sorting.
- 
